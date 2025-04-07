deefacc840b9a4277b22b7390db2f42d80832176
FOODOHOLIC <adarshkohli183@gmail.com>
2025-04-07 11:05:55 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express from 'express';
 import { connectDB } from './config/db.js';
+import path from "path";
 import productRoutes from "./routes/product.route.js";
 
 
@@ -10,9 +11,19 @@ import productRoutes from "./routes/product.route.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products",productRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, async()=>{
     await connectDB();
